33e383119d0179cba560adbae79cd7b0a8bdccb1
miquel <miquelvalero16@gmail.com>
2020-11-23 12:49:35 +0100

    first init

---
+++ routers/entrada.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const Entrada = require('../models/entrada')
+const router = new express.Router()
+
+router.post('/entradas', async (req, res) => {
+    const entrada = new Entrada(req.body)
+
+    try {
+        await entrada.save()
+        //res.status(201).send(test)
+        res.redirect('/');
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.get('/entradas', async (req, res) => {
+    try {
+        const entrada = await Entrada.find({})
+        res.send(entrada)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.get('/entradas/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const entrada = await Entrada.findById(_id)
+
+        if (!entrada) {
+            return res.status(404).send()
+        }
+
+        res.send(entrada)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.patch('/entradas/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['title', 'snippet', 'body']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const entrada = await Entrada.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!entrada) {
+            return res.status(404).send()
+        }
+
+        res.send(entrada)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.delete('/entradas/:id', async (req, res) => {
+    try {
+        const entrada = await Entrada.findByIdAndDelete(req.params.id)
+
+        if (!entrada) {
+            res.status(404).send()
+        }
+
+        res.send(entrada)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+module.exports = router
---
+++ src/app.js
@@ -0,0 +1,51 @@
+/* { ----------- EXPRESS ----------- } */
+const express = require('express');
+const app = express();
+
+/* { ----------- MODELS & ROUTERS ----------- } */
+require("./db/mongoose");
+const Entrada = require('./models/entrada');
+const entradaRouter = require('./routers/entrada');
+
+/* { ----------- PORT ----------- } */
+const port = process.env.PORT || 3000
+
+/* { ----------- APP LISTEN ----------- } */
+app.listen(port, ()=>{
+    console.log(`Listening to port ${port}`);
+});
+
+/* { ----------- EJS VIEW ENGINE ----------- } */
+app.set('view engine', 'ejs');
+
+/* { ----------- MIDDLEWARE ----------- } */
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log('new request made:');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+    next();
+});
+
+/* { ----------- RUTAS WEB ----------- } */
+app.get('/', async (req, res) => {
+    await Entrada.find({}).then((data) => {
+        res.render('blog', { entradas: data })
+    });
+});
+app.get('/crear-entrada', (req, res) => {
+    res.render('crear', {  })
+});
+
+/* { ----------- API ROUTING ----------- } */
+// EXAMPLE: app.use('/api', modelRouter)
+app.use('/api', entradaRouter);
+
+/* { ----------- EXPRESS ----------- } */
+app.use((req, res) => {
+    res.status(404).render('404', {});
+});
