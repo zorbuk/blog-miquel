312481bb716819750d320c59901b75bff62f63f4
miquel <miquelvalero16@gmail.com>
2020-11-25 09:15:08 +0100

    added: axios post

---
+++ public/js/crear.js
@@ -0,0 +1,21 @@
+    // ------------ BLOG ID
+    // const blogId = document.getElementById('bid').value;
+
+    // ------------ VALUES
+    let _title, _snippet, _bodyBlog
+
+    // ------------ CLICKABLE
+    const aSubmit = document.getElementById('submit');
+
+    // ------------ EVENT LISTENERS
+    aSubmit.addEventListener("click", ()=>{
+        _title = document.getElementById('title').value;
+        _snippet = document.getElementById('snippet').value;
+        _bodyBlog = document.getElementById('body').value;
+
+      axios.post(`/api/entradas/${blogId}`, {
+        "title": _title,
+        "snippet": _snippet,
+        "body": _bodyBlog
+        });
+    })
