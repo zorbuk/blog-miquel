30712f33210b16c59d58b9ddcc812f045fabf9b4
miquel <miquelvalero16@gmail.com>
2020-11-26 09:26:50 +0100

    fix: axios get

--- public/js/tiempo.js
+++ public/js/tiempo.js
@@ -3,9 +3,10 @@
 //REG: https://home.openweathermap.org/api_keys
 const readRequest = document.querySelector('#tiempo')
 
-readRequests.addEventListener('click', async (e)=>{
+readRequest.addEventListener('click', async (e)=>{
     e.preventDefault()
 
     const resultado = await axios.get('http://api.openweathermap.org/data/2.5/weather?id=3111933&appid=e927f468beb5e30dc31289b3be04ad74')
-    console.log('El tiempo en reus: ', resultado)
+
+    pTiempo.innerHTML = `El tiempo en reus: ${resultado}`;
 })
